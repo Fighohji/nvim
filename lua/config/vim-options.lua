98b9d64b2498d5bf2c61b7c3fdf95130c4143440
root <root@LAPTOP-EVP02MIV>
2024-02-07 22:05:16 +0800

    config

---
+++ lua/config/vim-options.lua
@@ -0,0 +1,24 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set smarttab")
+vim.cmd("set autoindent")
+
+vim.cmd("set clipboard=unnamedplus")
+
+vim.cmd("set relativenumber")
+
+vim.cmd("set history=1000")
+
+vim.cmd("set scrolloff=5")
+
+vim.cmd("set whichwrap+=<,>,h,l")
+
+vim.cmd("set noswapfile")
+
+vim.opt.smoothscroll = true
+
+vim.g.mapleader = " "
+
+
---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,16 @@
+return {
+  "nvimtools/none-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      source = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.clang_format,
+      },
+    })
+    vim.keymap.set('n', '<C-F>', vim.lsp.buf.format, {})
+  end
+}
